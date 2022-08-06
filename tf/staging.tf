d83645498966752ae58bfde0aa045437a51617e4
nugangha <52476525+nugangha@users.noreply.github.com>
2022-08-05 21:29:06 -0400

    Update staging.tf

--- tf/staging.tf
+++ tf/staging.tf
@@ -1,1 +1,154 @@
-# staging file
+resource "vault_generic_secret" "gateway_development" {
+  provider = vault.vault_dev
+  path     = "secret/development/gateway"
+
+  data_json = <<EOT
+{
+  "db_user":   "gateway",
+  "db_password": "10350819-4802-47ac-9476-6fa781e35cfd"
+}
+EOT
+}
+
+resource "vault_policy" "gateway_development" {
+  provider = vault.vault_dev
+  name     = "gateway-development"
+
+  policy = <<EOT
+path "secret/data/development/gateway" {
+    capabilities = ["list", "read"]
+}
+EOT
+}
+
+resource "vault_generic_endpoint" "gateway_development" {
+  provider             = vault.vault_dev
+  depends_on           = [vault_auth_backend.userpass_dev]
+  path                 = "auth/userpass/users/gateway-development"
+  ignore_absent_fields = true
+
+  data_json = <<EOT
+{
+  "policies": ["gateway-development"],
+  "password": "123-gateway-development"
+}
+EOT
+}
+
+
+resource "vault_generic_secret" "gateway_development" {
+  provider = vault.vault_stage
+  path     = "secret/development/gateway"
+
+  data_json = <<EOT
+{
+  "db_user":   "gateway",
+  "db_password": "10350819-4802-47ac-9476-6fa781e35cfd"
+}
+EOT
+}
+
+resource "vault_policy" "gateway_development" {
+  provider = vault.vault_stage
+  name     = "gateway-development"
+
+  policy = <<EOT
+path "secret/data/development/gateway" {
+    capabilities = ["list", "read"]
+}
+EOT
+}
+
+resource "vault_generic_endpoint" "gateway_development" {
+  provider             = vault.vault_stage
+  depends_on           = [vault_auth_backend.userpass_stage]
+  path                 = "auth/userpass/users/gateway-development"
+  ignore_absent_fields = true
+
+  data_json = <<EOT
+{
+  "policies": ["gateway-development"],
+  "password": "123-gateway-development"
+}
+EOT
+}
+
+
+resource "vault_generic_secret" "gateway_production" {
+  provider = vault.vault_prod
+  path     = "secret/production/gateway"
+
+  data_json = <<EOT
+{
+  "db_user":   "gateway",
+  "db_password": "33fc0cc8-b0e3-4c06-8cf6-c7dce2705329"
+}
+EOT
+}
+
+resource "vault_policy" "gateway_production" {
+  provider = vault.vault_prod
+  name     = "gateway-production"
+
+  policy = <<EOT
+path "secret/data/production/gateway" {
+    capabilities = ["list", "read"]
+}
+EOT
+}
+
+resource "vault_generic_endpoint" "gateway_production" {
+  provider             = vault.vault_prod
+  depends_on           = [vault_auth_backend.userpass_prod]
+  path                 = "auth/userpass/users/gateway-production"
+  ignore_absent_fields = true
+
+  data_json = <<EOT
+{
+  "policies": ["gateway-production"],
+  "password": "123-gateway-production"
+}
+EOT
+}
+
+resource "docker_container" "gateway_development" {
+  image = "form3tech-oss/platformtest-gateway"
+  name  = "gateway_development"
+
+  env = [
+    "VAULT_ADDR=http://vault-development:8200",
+    "VAULT_USERNAME=gateway-development",
+    "VAULT_PASSWORD=123-gateway-development",
+    "ENVIRONMENT=development"
+  ]
+
+  networks_advanced {
+    name = "vagrant_development"
+  }
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+
+
+resource "docker_container" "gateway_production" {
+  image = "form3tech-oss/platformtest-gateway"
+  name  = "gateway_production"
+
+  env = [
+    "VAULT_ADDR=http://vault-production:8200",
+    "VAULT_USERNAME=gateway-production",
+    "VAULT_PASSWORD=123-gateway-production",
+    "ENVIRONMENT=production"
+  ]
+
+  networks_advanced {
+    name = "vagrant_production"
+  }
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
