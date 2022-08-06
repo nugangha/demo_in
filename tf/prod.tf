fa90fa9f560a5ec34450893abe4ba9cfc6ca9696
nugangha <52476525+nugangha@users.noreply.github.com>
2022-08-05 21:31:38 -0400

    Update prod.tf

--- tf/prod.tf
+++ tf/prod.tf
@@ -1,1 +1,152 @@
-# prod file
+resource "vault_generic_secret" "account_development" {
+  provider = vault.vault_dev
+  path     = "secret/development/account"
+
+  data_json = <<EOT
+{
+  "db_user":   "account",
+  "db_password": "965d3c27-9e20-4d41-91c9-61e6631870e7"
+}
+EOT
+}
+
+resource "vault_policy" "account_development" {
+  provider = vault.vault_dev
+  name     = "account-development"
+
+  policy = <<EOT
+path "secret/data/development/account" {
+    capabilities = ["list", "read"]
+}
+EOT
+}
+
+resource "vault_generic_endpoint" "account_development" {
+  provider             = vault.vault_dev
+  depends_on           = [vault_auth_backend.userpass_dev]
+  path                 = "auth/userpass/users/account-development"
+  ignore_absent_fields = true
+
+  data_json = <<EOT
+{
+  "policies": ["account-development"],
+  "password": "123-account-development"
+}
+EOT
+}
+
+resource "vault_generic_secret" "account_development" {
+  provider = vault.vault_stage
+  path     = "secret/development/account"
+
+  data_json = <<EOT
+{
+  "db_user":   "account",
+  "db_password": "965d3c27-9e20-4d41-91c9-61e6631870e7"
+}
+EOT
+}
+
+resource "vault_policy" "account_development" {
+  provider = vault.vault_stage
+  name     = "account-development"
+
+  policy = <<EOT
+path "secret/data/development/account" {
+    capabilities = ["list", "read"]
+}
+EOT
+}
+
+resource "vault_generic_endpoint" "account_development" {
+  provider             = vault.vault_stage
+  depends_on           = [vault_auth_backend.userpass_stage]
+  path                 = "auth/userpass/users/account-development"
+  ignore_absent_fields = true
+
+  data_json = <<EOT
+{
+  "policies": ["account-development"],
+  "password": "123-account-development"
+}
+EOT
+}
+
+
+resource "vault_generic_secret" "account_production" {
+  provider = vault.vault_prod
+  path     = "secret/production/account"
+
+  data_json = <<EOT
+{
+  "db_user":   "account",
+  "db_password": "396e73e7-34d5-4b0a-ae1b-b128aa7f9977"
+}
+EOT
+}
+
+resource "vault_policy" "account_production" {
+  provider = vault.vault_prod
+  name     = "account-production"
+
+  policy = <<EOT
+path "secret/data/production/account" {
+    capabilities = ["list", "read"]
+}
+EOT
+}
+
+resource "vault_generic_endpoint" "account_production" {
+  provider             = vault.vault_prod
+  depends_on           = [vault_auth_backend.userpass_prod]
+  path                 = "auth/userpass/users/account-production"
+  ignore_absent_fields = true
+
+  data_json = <<EOT
+{
+  "policies": ["account-production"],
+  "password": "123-account-production"
+}
+EOT
+}
+
+resource "docker_container" "account_development" {
+  image = "form3tech-oss/platformtest-account"
+  name  = "account_development"
+
+  env = [
+    "VAULT_ADDR=http://vault-development:8200",
+    "VAULT_USERNAME=account-development",
+    "VAULT_PASSWORD=123-account-development",
+    "ENVIRONMENT=development"
+  ]
+
+  networks_advanced {
+    name = "vagrant_development"
+  }
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+resource "docker_container" "account_production" {
+  image = "form3tech-oss/platformtest-account"
+  name  = "account_production"
+
+  env = [
+    "VAULT_ADDR=http://vault-production:8200",
+    "VAULT_USERNAME=account-production",
+    "VAULT_PASSWORD=123-account-production",
+    "ENVIRONMENT=production"
+  ]
+
+  networks_advanced {
+    name = "vagrant_production"
+  }
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
